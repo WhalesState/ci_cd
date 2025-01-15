d83f620eca3a9aaadf95c2bde23d474e3909ccb0
Randolph W. Aarseth II <bioblazepayne@gmail.com>
2025-01-15 15:52:51 -0700

    modified a few things

--- cicd/scripts/changelog2txt.js
+++ cicd/scripts/changelog2txt.js
@@ -24,7 +24,7 @@ function generateChangelogText(jsonFilePath) {
 
         // Template: Header Section
         let changelogText = `
-Changelog: ${baseBranch} → ${currentBranch}
+Changelog: ${baseBranch} -> ${currentBranch}
 
 Summary:
 - Version: ${version["major"]}.${version["minor"]}.${version["patch"]}
